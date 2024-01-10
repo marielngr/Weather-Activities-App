25eb0771da52a67e2ae9193edb10ed2c36ee1599
Vanessa Ehrlein <vanessa.ehrlein@posteo.de>
2024-01-10 10:46:20 +0100

    add Activity-Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,3 +1,19 @@
 export default function Form() {
-  return <h2>Test</h2>;
+  return (
+    <form>
+      <h2>Add new Activity:</h2>
+
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" name="name"></input>
+
+      <label htmlFor="activity-weather">Good-weather activity:</label>
+      <input
+        type="checkbox"
+        id="activity-weather"
+        name="activity-weather"
+      ></input>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
