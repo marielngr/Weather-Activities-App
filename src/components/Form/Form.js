8a0cc036a4e910275dbb0b5b977745adb72a2f00
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-01-11 09:48:19 +0100

    add css

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,13 @@
+import "./Form.css";
+
 export default function Form({ onAddActivity }) {
   function getData(event) {
     event.preventDefault();
     const name = event.target.name.value;
     const isWeatherChecked = event.target.isForGoodWeather.checked;
+
     onAddActivity(name, isWeatherChecked);
+
     event.target.reset();
     event.target.name.focus();
   }
@@ -11,16 +15,18 @@ export default function Form({ onAddActivity }) {
   return (
     <form onSubmit={getData}>
       <h2>Add new Activity:</h2>
-
-      <label htmlFor="name">Name:</label>
-      <input type="text" id="name" name="name"></input>
-
-      <label htmlFor="isForGoodWeather">Good-weather activity:</label>
-      <input
-        type="checkbox"
-        id="isForGoodWeather"
-        name="isForGoodWeather"
-      ></input>
+      <div className="inputContainer">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name"></input>
+      </div>
+      <div class="inputContainer">
+        <label htmlFor="isForGoodWeather">Good-weather activity:</label>
+        <input
+          type="checkbox"
+          id="isForGoodWeather"
+          name="isForGoodWeather"
+        ></input>
+      </div>
 
       <button type="submit">Submit</button>
     </form>
